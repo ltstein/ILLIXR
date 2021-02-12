ddd9c1ce8c02af8ae22bfddd533f301673a63278
Giordano Salvador <73959795+e3m3@users.noreply.github.com>
2021-02-12 09:34:47 -0500

    Implementing constructor forwarding allocation technique (#32)

--- common/data_format.hpp
+++ common/data_format.hpp
@@ -54,123 +54,138 @@ namespace ILLIXR {
         ullong timestamp;
     } rgb_depth_type;
 
-    // Values needed to initialize the IMU integrator
-    typedef struct {
-        double gyro_noise;
-        double acc_noise;
-        double gyro_walk;
-        double acc_walk;
-        Eigen::Matrix<double,3,1> n_gravity;
-        double imu_integration_sigma;
-        double nominal_rate;
-    } imu_params;
+	// Values needed to initialize the IMU integrator
+	typedef struct {
+		double gyro_noise;
+		double acc_noise;
+		double gyro_walk;
+		double acc_walk;
+		Eigen::Matrix<double,3,1> n_gravity;
+		double imu_integration_sigma;
+		double nominal_rate;
+	} imu_params;
 
-    // IMU biases, initialization params, and slow pose needed by the IMU integrator
-    struct imu_integrator_input : public switchboard::event {
-        double last_cam_integration_time;
-        double t_offset;
-        imu_params params;
-        
-        Eigen::Vector3d biasAcc;
-        Eigen::Vector3d biasGyro;
-        Eigen::Matrix<double,3,1> position;
-        Eigen::Matrix<double,3,1> velocity;
-        Eigen::Quaterniond quat;
-        imu_integrator_input(
-                             double last_cam_integration_time_,
-                             double t_offset_,
-                             imu_params params_,
-                             Eigen::Vector3d biasAcc_,
-                             Eigen::Vector3d biasGyro_,
-                             Eigen::Matrix<double,3,1> position_,
-                             Eigen::Matrix<double,3,1> velocity_,
-                             Eigen::Quaterniond quat_
-                             )
-            : last_cam_integration_time{last_cam_integration_time_}
-            , t_offset{t_offset_}
-            , params{params_}
-            , biasAcc{biasAcc_}
-            , biasGyro{biasGyro_}
-            , position{position_}
-            , velocity{velocity_}
-            , quat{quat_}
-        { }
-    };
+	// IMU biases, initialization params, and slow pose needed by the IMU integrator
+	struct imu_integrator_input : public switchboard::event {
+		double last_cam_integration_time;
+		double t_offset;
+		imu_params params;
+		
+		Eigen::Vector3d biasAcc;
+		Eigen::Vector3d biasGyro;
+		Eigen::Matrix<double,3,1> position;
+		Eigen::Matrix<double,3,1> velocity;
+		Eigen::Quaterniond quat;
+		imu_integrator_input(
+							 double last_cam_integration_time_,
+							 double t_offset_,
+							 imu_params params_,
+							 Eigen::Vector3d biasAcc_,
+							 Eigen::Vector3d biasGyro_,
+							 Eigen::Matrix<double,3,1> position_,
+							 Eigen::Matrix<double,3,1> velocity_,
+							 Eigen::Quaterniond quat_
+							 )
+			: last_cam_integration_time{last_cam_integration_time_}
+			, t_offset{t_offset_}
+			, params{params_}
+			, biasAcc{biasAcc_}
+			, biasGyro{biasGyro_}
+			, position{position_}
+			, velocity{velocity_}
+			, quat{quat_}
+		{ }
+	};
 
-    // Output of the IMU integrator to be used by pose prediction
-    struct imu_raw_type : switchboard::event{
-        // Biases from the last two IMU integration iterations used by RK4 for pose predict
-        Eigen::Matrix<double,3,1> w_hat;
-        Eigen::Matrix<double,3,1> a_hat;
-        Eigen::Matrix<double,3,1> w_hat2;
-        Eigen::Matrix<double,3,1> a_hat2;
+	// Output of the IMU integrator to be used by pose prediction
+	struct imu_raw_type : switchboard::event{
+		// Biases from the last two IMU integration iterations used by RK4 for pose predict
+		Eigen::Matrix<double,3,1> w_hat;
+		Eigen::Matrix<double,3,1> a_hat;
+		Eigen::Matrix<double,3,1> w_hat2;
+		Eigen::Matrix<double,3,1> a_hat2;
 
-        // Faster pose propagated forwards by the IMU integrator
-        Eigen::Matrix<double,3,1> pos;
-        Eigen::Matrix<double,3,1> vel;
-        Eigen::Quaterniond quat;
-        time_type imu_time;
-        imu_raw_type(Eigen::Matrix<double,3,1> w_hat_,
-                     Eigen::Matrix<double,3,1> a_hat_,
-                     Eigen::Matrix<double,3,1> w_hat2_,
-                     Eigen::Matrix<double,3,1> a_hat2_,
-                     Eigen::Matrix<double,3,1> pos_,
-                     Eigen::Matrix<double,3,1> vel_,
-                     Eigen::Quaterniond quat_,
-                     time_type imu_time_)
-            : w_hat{w_hat_}
-            , a_hat{a_hat_}
-            , w_hat2{w_hat2_}
-            , a_hat2{a_hat2_}
-            , pos{pos_}
-            , vel{vel_}
-            , quat{quat_}
-            , imu_time{imu_time_}
-        { }
-    };
+		// Faster pose propagated forwards by the IMU integrator
+		Eigen::Matrix<double,3,1> pos;
+		Eigen::Matrix<double,3,1> vel;
+		Eigen::Quaterniond quat;
+		time_type imu_time;
+		imu_raw_type(Eigen::Matrix<double,3,1> w_hat_,
+					 Eigen::Matrix<double,3,1> a_hat_,
+					 Eigen::Matrix<double,3,1> w_hat2_,
+					 Eigen::Matrix<double,3,1> a_hat2_,
+					 Eigen::Matrix<double,3,1> pos_,
+					 Eigen::Matrix<double,3,1> vel_,
+					 Eigen::Quaterniond quat_,
+					 time_type imu_time_)
+			: w_hat{w_hat_}
+			, a_hat{a_hat_}
+			, w_hat2{w_hat2_}
+			, a_hat2{a_hat2_}
+			, pos{pos_}
+			, vel{vel_}
+			, quat{quat_}
+			, imu_time{imu_time_}
+		{ }
+	};
 
-    struct pose_type : switchboard::event {
-        time_type sensor_time; // Recorded time of sensor data ingestion
-        Eigen::Vector3f position;
-        Eigen::Quaternionf orientation;
-        pose_type() { }
-        pose_type(time_type sensor_time_,
-                  Eigen::Vector3f position_,
-                  Eigen::Quaternionf orientation_)
-            : sensor_time{sensor_time_}
-            , position{position_}
-            , orientation{orientation_}
-        { }
-    };
+	struct pose_type : switchboard::event {
+		time_type sensor_time; // Recorded time of sensor data ingestion
+		Eigen::Vector3f position;
+		Eigen::Quaternionf orientation;
+		pose_type() { }
+		pose_type(time_type sensor_time_,
+				  Eigen::Vector3f position_,
+				  Eigen::Quaternionf orientation_)
+			: sensor_time{sensor_time_}
+			, position{position_}
+			, orientation{orientation_}
+		{ }
+	};
 
-    typedef struct {
-        pose_type pose;
-        time_type predict_computed_time; // Time at which the prediction was computed
-        time_type predict_target_time; // Time that prediction targeted.
-    } fast_pose_type;
+	typedef struct {
+		pose_type pose;
+		time_type predict_computed_time; // Time at which the prediction was computed
+		time_type predict_target_time; // Time that prediction targeted.
+	} fast_pose_type;
 
-    // Using arrays as a swapchain
-    // Array of left eyes, array of right eyes
-    // This more closely matches the format used by Monado
-    struct rendered_frame : switchboard::event {
-        GLuint texture_handles[2]; // Does not change between swaps in swapchain
-        GLuint swap_indices[2]; // Which element of the swapchain
-        fast_pose_type render_pose; // The pose used when rendering this frame.
-        std::chrono::time_point<std::chrono::system_clock> sample_time;
-        std::chrono::time_point<std::chrono::system_clock> render_time;
-    };
+	// Using arrays as a swapchain
+	// Array of left eyes, array of right eyes
+	// This more closely matches the format used by Monado
+	struct rendered_frame : switchboard::event {
+		GLuint texture_handles[2]; // Does not change between swaps in swapchain
+		GLuint swap_indices[2]; // Which element of the swapchain
+		fast_pose_type render_pose; // The pose used when rendering this frame.
+		std::chrono::time_point<std::chrono::system_clock> sample_time;
+		std::chrono::time_point<std::chrono::system_clock> render_time;
+		rendered_frame() { }
+		rendered_frame(GLuint texture_handles_[2],
+		               GLuint swap_indices_[2],
+		               fast_pose_type render_pose_,
+                       std::chrono::time_point<std::chrono::system_clock> sample_time_,
+                       std::chrono::time_point<std::chrono::system_clock> render_time_)
+            : render_pose(render_pose_)
+            , sample_time(sample_time_)
+            , render_time(render_time_)
+        {
+            texture_handles[0]  = texture_handles_[0];
+            texture_handles[1]  = texture_handles_[1];
+            swap_indices[0]     = swap_indices_[0];
+            swap_indices[1]     = swap_indices_[1];
+        }
+	};
 
-    typedef struct {
-        int seq;
-    } hologram_input;
+	typedef struct {
+		int seq;
+	} hologram_input;
 
-    typedef struct {
-        int dummy;
-    } hologram_output;
+	typedef struct {
+		int dummy;
+	} hologram_output;
 
-    typedef struct {
-        int seq;        
-    } imu_integrator_seq;
+	typedef struct {
+		int seq;		
+	} imu_integrator_seq;
 
     /* I use "accel" instead of "3-vector" as a datatype, because
     this checks that you meant to use an acceleration in a certain
